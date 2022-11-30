391b12c01156f6e236dbe2c0a9c153ea864b956d
JuanZoran <1430359574@qq.com>
2022-11-30 17:59:38 +0800

    init

---
+++ lua/module/init.lua
@@ -0,0 +1,44 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- Only required if you have packer configured as `opt`
+-- vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+augroup end
+]]
+-- Have packer use a popup window
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+    return
+end
+
+packer.init {
+    display = {
+        open_fn = function()
+            return require("packer.util").float { border = "rounded" }
+        end,
+    },
+}
+
+
+return packer.startup(function(use)
+    use 'wbthomason/packer.nvim'
+    if packer_bootstrap then
+        require('packer').sync()
+    end
+end)
