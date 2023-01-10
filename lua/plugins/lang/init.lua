ac05381a73203d5998e607ba84b8d46ef3d9009e
JuanZoran <1430359574@qq.com>
2023-01-10 21:05:55 +0800

    feat: remove lsp_signature, noice has done that

--- lua/plugins/lang/init.lua
+++ lua/plugins/lang/init.lua
@@ -51,7 +51,6 @@ use {
     "williamboman/mason-lspconfig.nvim", -- make bridge between lspconfig and mason
     "neovim/nvim-lspconfig", -- official lspconfig
     "glepnir/lspsaga.nvim", -- pretty ui for [code-action | hover-text | ....]
-    'ray-x/lsp_signature.nvim',
     -- NOTE  Dap
     -- 'jayp0521/mason-nvim-dap.nvim',
     -- 'mfussenegger/nvim-dap',
