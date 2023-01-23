cba4d610fd6de9495b1da534bc0e3a776920f6dd
JuanZoran <1430359574@qq.com>
2023-01-23 11:09:14 +0800

    feat: add smooth cursor mode

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,13 +1,84 @@
-local View = vim.api.nvim_create_augroup('View', { clear = true })
-vim.api.nvim_create_autocmd('BufRead', {
-    group = View,
+local api = vim.api
+
+local group = api.nvim_create_augroup('UserDefine', { clear = true })
+api.nvim_create_autocmd('BufRead', {
+    group = group,
     command = [[silent! loadview]]
 })
 
-vim.api.nvim_create_autocmd({ 'BufWrite', 'QuitPre' }, {
-    group = View,
+api.nvim_create_autocmd({ 'BufWrite', 'QuitPre' }, {
+    group = group,
     command = [[silent! mkview | SessionSave]]
 })
+
+-- Don't auto comment when o
+api.nvim_create_autocmd("FileType", { command = [[set formatoptions-=cro]] })
+
+
+api.nvim_create_autocmd("TextYankPost", {
+    group = group,
+    callback = function()
+        vim.highlight.on_yank {
+            higroup = "IncSearch",
+            timeout = 250,
+        }
+    end,
+})
+
+
+-- NOTE  Snip Utility
+api.nvim_create_autocmd("BufEnter", {
+    group = group,
+    pattern = "*/snips/*.lua",
+    callback = function()
+        vim.keymap.set('n', '<CR>', '/-- End Snippets --<CR>:noh<CR>2<Up>o', { silent = true, buffer = true })
+    end,
+})
+
+
+-- INFO 中文输入法切换的问题
+if vim.fn.executable('fcitx5-remote') then
+    local state = ''
+    api.nvim_create_autocmd('InsertLeave', {
+        group = group,
+        pattern = '*',
+        callback = function()
+            vim.opt_local.cursorline = true
+            state = io.popen('fcitx5-remote'):read('*a')
+            os.execute('fcitx5-remote -c')
+        end,
+    })
+
+    api.nvim_create_autocmd('InsertEnter', {
+        group = group,
+        pattern = '*',
+        callback = function()
+            vim.opt_local.cursorline = false
+            if state == '2\n' then
+                os.execute('fcitx5-remote -o')
+            end
+        end,
+    })
+end
+
+
+-- INFO cursorline
+api.nvim_create_autocmd('FileType', {
+    group = group,
+    pattern = "TelescopePrompt",
+    callback = function()
+        vim.opt_local.cursorline = false
+    end,
+})
+
+-- INFO : store Position
+-- api.nvim_create_autocmd(
+--     "BufReadPost",
+--     {
+--         group = api.nvim_create_augroup('Position', { clear = true }),
+--         command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]]
+--     }
+-- )
 
 -- -- 设置firenvim的大小
 -- vim.cmd [[
@@ -19,78 +90,3 @@ vim.api.nvim_create_autocmd({ 'BufWrite', 'QuitPre' }, {
 -- autocmd UIEnter * call OnUIEnter(deepcopy(v:event))
 -- ]]
 
--- Don't auto comment when o
-vim.api.nvim_create_autocmd("FileType", { command = [[set formatoptions-=cro]] })
-
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-    callback = function()
-        vim.highlight.on_yank {
-            higroup = "IncSearch",
-            timeout = 250,
-        }
-    end,
-})
-
-
--- NOTE  Snip Utility
-local snip = vim.api.nvim_create_augroup("CodeSnip", { clear = true })
-vim.api.nvim_create_autocmd("BufEnter", {
-    group = snip,
-    pattern = "*/snips/*.lua",
-    callback = function()
-        vim.keymap.set('n', '<CR>', '/-- End Snippets --<CR>:noh<CR>2<Up>o', { silent = true, buffer = true })
-    end,
-})
-
-
--- INFO 中文输入法切换的问题
-if vim.fn.executable('fcitx5-remote') then
-    local fcitx = vim.api.nvim_create_augroup("Fcitx5", { clear = true })
-    local state = ''
-    vim.api.nvim_create_autocmd('InsertLeave', {
-        group = fcitx,
-        pattern = '*',
-        callback = function()
-            state = io.popen('fcitx5-remote'):read('*a')
-            os.execute('fcitx5-remote -c')
-        end,
-    })
-
-    vim.api.nvim_create_autocmd('InsertEnter', {
-        group = fcitx,
-        pattern = '*',
-        callback = function()
-            if state == '2\n' then
-                os.execute('fcitx5-remote -o')
-            end
-        end,
-    })
-end
-
--- INFO : store Position
--- vim.api.nvim_create_autocmd(
---     "BufReadPost",
---     {
---         group = vim.api.nvim_create_augroup('Position', { clear = true }),
---         command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]]
---     }
--- )
-
--- INFO cursorline
-local group = vim.api.nvim_create_augroup("CursorLineControl", { clear = true })
-local set_cursorline = function(event, value, pattern)
-    vim.api.nvim_create_autocmd(event, {
-        group = group,
-        pattern = pattern,
-        callback = function()
-            vim.opt_local.cursorline = value
-        end,
-    })
-end
-
--- set_cursorline("WinLeave", false)
-set_cursorline("InsertEnter", false)
-set_cursorline("InsertLeave", true)
--- set_cursorline("WinEnter", true)
-set_cursorline("FileType", false, "TelescopePrompt")
--- lua/plugins/ui/smooth.lua
+++ lua/plugins/ui/smooth.lua
@@ -1,26 +1,3 @@
-require('cinnamon').setup {
-    default_keymaps = false, -- Create default keymaps.
-    extra_keymaps = false,
-    override_keymaps = false, -- whether to force mapping
-    hide_cursor = true,
-    -- max_length = 500,
-    -- scroll_limit = 200,
-}
-
--- Syntax: t[keys] = {function, {function arguments}}
--- Use the "sine" easing function
--- Use the "circular" easing function
--- t['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '500', [['circular']]}}
--- t['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '500', [['circular']]}}
--- Pass "nil" to disable the easing animation (constant scrolling speed)
--- t['<C-y>'] = {'scroll', {'-0.10', 'false', '100', nil}}
--- t['<C-e>'] = {'scroll', { '0.10', 'false', '100', nil}}
--- When no easing function is provided the default easing function (in this case "quadratic") will be used
--- t['zt']    = {'zt', {'300'}}
--- t['zz']    = {'zz', {'300'}}
--- t['zb']    = {'zb', {'300'}}
-
--- local distance = 10
 local specs = require('specs')
 local show = specs.show_specs
 specs.setup {
@@ -40,6 +17,45 @@ specs.setup {
     },
 }
 
+if 1 == 1 then
+    require('cinnamon').setup {
+        default_keymaps = false, -- Create default keymaps.
+        extra_keymaps = false,
+        override_keymaps = false, -- whether to force mapping
+        hide_cursor = true,
+        default_delay = 8,        -- The default delay (in ms) between each line when scrolling.
+    }
+
+    vim.keymap.set({ 'n', 'x' }, 'I', "<Cmd>lua Scroll('<C-u>', 1, 1)<CR><Cmd>lua require('specs').show_specs()<CR>")
+    vim.keymap.set({ 'n', 'x' }, 'K', "<Cmd>lua Scroll('<C-d>', 1, 1)<CR><Cmd>lua require('specs').show_specs()<CR>")
+
+-- elseif mode == 2 then
+    require('smoothcursor').setup {
+        fancy = {
+            -- head = { cursor = "▷", texthl = "SmoothCursor", linehl = 'CursorLine' },
+            enable = true
+        },
+
+        speed = 30, -- max is 100 to stick to your current position
+        intervals = 35, -- tick intervalI
+        disable_float_win = true, -- disable on float window
+        -- linehl = 'CursorLine',
+    }
+    -- vim.keymap.set({ 'n', 'x' }, 'I', function()
+    --     vim.defer_fn(show, 10)
+    --     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-u>', true, false, true), 'n', false)
+    --     vim.api.nvim_feedkeys('zz', 'n', false)
+    -- end)
+
+    -- vim.keymap.set({ 'n', 'x' }, 'K', function()
+    --     vim.defer_fn(show, 10)
+    --     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-d>', true, false, true), 'n', false)
+    --     vim.api.nvim_feedkeys('zz', 'n', false)
+    -- end)
+end
+
+
+
 local set = require('util.map').set
 set {
     mode = { 'n', 'x', 'o' }, -- be appended to other operator
@@ -48,7 +64,6 @@ set {
         { "k", "j" },
         { "j", "h" },
         { "h", "i" },
-        -- { "H", "I" },
     }
 }
 
@@ -56,8 +71,8 @@ set {
 set {
     mode = 'o',
     map = {
-        {'L', '$'},
-        {'J', '0'},
+        { 'L', '$' },
+        { 'J', '0' },
     }
 }
 
@@ -72,8 +87,6 @@ vim.keymap.set('n', 'A', function()
     vim.api.nvim_feedkeys('A', 'n', false)
 end)
 
-vim.keymap.set({ 'n', 'x' }, 'I', "<Cmd>lua Scroll('<C-u>', 1, 1)<CR><Cmd>lua require('specs').show_specs()<CR>")
-vim.keymap.set({ 'n', 'x' }, 'K', "<Cmd>lua Scroll('<C-d>', 1, 1)<CR><Cmd>lua require('specs').show_specs()<CR>")
 
 vim.keymap.set('n', 'cc', function()
     vim.defer_fn(show, 10)
@@ -96,16 +109,18 @@ end)
 vim.keymap.set({ 'n', 'x' }, 'J', function()
     if vim.fn.col('.') ~= 1 then
         vim.defer_fn(show, 10)
+        vim.fn.cursor { vim.fn.line('.'), 1 }
     end
-    vim.fn.cursor { vim.fn.line('.'), 1 }
 end)
+
 
 vim.keymap.set({ 'n', 'x' }, 'L', function()
     if vim.fn.col('.') ~= vim.fn.col('$') - 1 then
         vim.defer_fn(show, 10)
+        vim.fn.cursor { vim.fn.line('.'), vim.fn.col('$') }
     end
-    vim.fn.cursor { vim.fn.line('.'), vim.fn.col('$') }
 end)
+
 
 
 vim.api.nvim_create_autocmd('WinEnter', {
@@ -115,31 +130,3 @@ vim.api.nvim_create_autocmd('WinEnter', {
         end
     end
 })
-
--- LSP_KEYMAPS:
--- LSP go-to-definition:
--- vim.keymap.set('n', 'gd', "<Cmd>lua Scroll('definition')<CR>")
-
--- LSP go-to-declaration:
--- vim.keymap.set('n', 'gD', "<Cmd>lua Scroll('declaration')<CR>")
--- require("smoothcursor").setup {
---     fancy = {
---         enable = true,
---     },
---     -- type = "exp",         -- define cursor movement calculate function, "default" or "exp" (exponential).
---     -- enabled_filetypes = {
---     --     'lua',
---     -- },
---     disabled_filetypes = {
---         'specs',
---     }
--- }
--- vim.keymap.set('n', 'n', function()
---     Scroll('n', 1)
---     show()
--- end)
---
--- vim.keymap.set('n', 'N', function()
---     Scroll('N', 1)
---     show()
--- end)
