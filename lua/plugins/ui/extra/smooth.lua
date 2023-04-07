c72d2e8cdb0780b34c03133da715c8253eedd7ed
JuanZoran <1430359574@qq.com>
2023-04-07 15:43:36 +0800

    refactor: more animation style for cursor motion

--- lua/plugins/ui/extra/smooth.lua
+++ lua/plugins/ui/extra/smooth.lua
@@ -10,30 +10,62 @@ util.map {
     { mode = 'o', 'J', 'g^' },
 }
 
-local function keys()
-    local show = function() require 'specs'.show_specs() end
+
+local function config()
     local api = vim.api
     local fn = vim.fn
-    local function feedkey(key)
+    local specs = require 'specs'
+    specs.setup {
+        show_jumps = false,
+        popup      = {
+            delay_ms = 0,  -- delay before popup displays
+            inc_ms   = 8,  -- time increments used for fade/resize effects
+            blend    = 40, -- starting blend, between 0-100 (fully transparent), see :h winblend
+            width    = 13,
+            winhl    = 'Cursor',
+            fader    = require 'specs'.exp_fader,
+            resizer  = require 'specs'.slide_resizer,
+        },
+    }
+
+    local show = specs.show_specs
+    local feedkey = function(key, func)
+        func = func or show
         return function()
             -- api.nvim_feedkeys(api.nvim_replace_termcodes(key, true, true, true), 'n', false)
-            api.nvim_feedkeys(key, 'n', false)
-            vim.defer_fn(show, 10)
+            api.nvim_feedkeys(key, 'n', true)
+            vim.defer_fn(func, 10)
         end
     end
+
     local nx = { 'n', 'x' }
-    return {
+    local long_jump = function()
+        specs.show_specs {
+            -- delay_ms = 10, -- delay before popup displays
+            inc_ms  = 8,  -- time increments used for fade/resize effects
+            blend   = 10, -- starting blend, between 0-100 (fully transparent), see :h winblend
+            width   = 25,
+            winhl   = 'Cursor',
+            fader   = specs.linear_fader,
+            resizer = specs.shrink_resizer,
+        }
+    end
+
+    util.map {
         { 'H',  feedkey 'I' },
         { 'A',  feedkey 'A' },
-        { 'cc', feedkey 'cc' },
-        { 'G',  feedkey 'G',  mode = nx },
-        { 'gg', feedkey 'gg', mode = nx },
+        { 'cc', feedkey('cc', long_jump) },
+        { 'G',  feedkey('G', long_jump),  mode = nx },
+        { 'gg', feedkey('gg', long_jump), mode = nx },
         {
             mode = nx,
             'J',
             function()
                 local col = (api.nvim_get_current_line()):find '%S'
-                if col ~= vim.fn.col '.' then feedkey(vim.wo.wrap and 'g^' or '^')() end
+                if col and col ~= vim.fn.col '.' then
+                    api.nvim_feedkeys(vim.wo.wrap and 'g^' or '^', 'n', true)
+                    vim.defer_fn(show, 10)
+                end
             end,
         },
         {
@@ -42,51 +74,35 @@ local function keys()
             function()
                 local _cur = fn.col '.'
                 local _end = fn.col '$' - 1
-                if _end ~= 0 and _cur ~= _end then feedkey(vim.wo.wrap and 'g$' or '$')() end
+                if _end ~= 0 and _cur ~= _end then
+                    api.nvim_feedkeys(vim.wo.wrap and 'g$' or '$', 'n', true)
+                    vim.defer_fn(show, 10)
+                end
             end,
         },
     }
 end
 
+
+
 return {
     'JuanZoran/specs.nvim',
-    keys = keys,
-    opts = function()
-        return {
-           show_jumps      = false,
-            popup           = {
-                delay_ms = 0,  -- delay before popup displays
-                inc_ms   = 8,  -- time increments used for fade/resize effects
-                blend    = 40, -- starting blend, between 0-100 (fully transparent), see :h winblend
-                width    = 13,
-                winhl    = 'Cursor',
-                fader    = require 'specs'.linear_fader,
-                resizer  = require 'specs'.shrink_resizer,
-            },
-            ignore_buftypes = { nofile = true },
-        }
-    end,
+    config = config,
     dependencies = {
-        {
-            'gen740/SmoothCursor.nvim',
-            keys = {
-                { 'I', '<C-u>zz' },
-                { 'K', '<C-d>zz' },
+        'gen740/SmoothCursor.nvim',
+        opts = {
+            autostart = true,
+            speed = 30,               -- max is 100 to stick to your current position
+            intervals = 30,           -- tick intervalI
+            disable_float_win = true, -- disable on float window
+            -- linehl = 'CursorLine',
+            disabled_filetypes = {
+                'alpha',
+                'TelescopePrompt'
             },
-            opts = {
-                autostart = true,
-                speed = 30,               -- max is 100 to stick to your current position
-                intervals = 30,           -- tick intervalI
-                disable_float_win = true, -- disable on float window
-                -- linehl = 'CursorLine',
-                disabled_filetypes = {
-                    'alpha',
-                    'TelescopePrompt'
-                },
-                fancy = {
-                    head = { cursor = '▷', texthl = 'SmoothCursor', linehl = nil },
-                    enable = true,
-                },
+            fancy = {
+                head = { cursor = '▷', texthl = 'SmoothCursor', linehl = nil },
+                enable = true,
             },
         },
     },
